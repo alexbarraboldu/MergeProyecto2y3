1352bb2d691fd41f9228195517d488bbebf4f70f
Alex Barra <48607651+alexbarraboldu@users.noreply.github.com>
2020-01-26 00:23:32 +0100

    1.0.0
    Se puede jugar. No guarda la partida. Solo funciona la función procedural.

---
+++ Project1/Structures.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Constants.h"
+
+struct Position2D
+{
+	int X, Y;
+};
+
+struct Player
+{
+	char skin = 'O';
+	Position2D pos;
+};
+
+struct Enemy
+{
+	char skin = 'X';
+	Position2D pos;
+};
+
+struct room
+{
+	room * north = nullptr;
+	room * south = nullptr;
+	room * east = nullptr;
+	room * west = nullptr;
+	std::vector<Enemy> enemyList;
+	Player player;
+	charbivector map;
+	int size;
+	int startEnemyNum;
+};
+
